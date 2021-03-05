71af07d332513598fca4195f04aba7833b75921b
minotaur487 <63748624+minotaur487@users.noreply.github.com>
2021-03-05 10:39:25 -0500

    first commit

---
+++ Wurd/StudentTextEditor.h
@@ -0,0 +1,36 @@
+#ifndef STUDENTTEXTEDITOR_H_
+#define STUDENTTEXTEDITOR_H_
+
+#include "TextEditor.h"
+#include <list>
+
+class Undo;
+
+class StudentTextEditor : public TextEditor {
+public:
+
+	StudentTextEditor(Undo* undo);
+	~StudentTextEditor();
+	bool load(std::string file);
+	bool save(std::string file);
+	void reset();
+	void move(Dir dir);
+	void del();
+	void backspace();
+	void insert(char ch);
+	void enter();
+	void getPos(int& row, int& col) const;
+	int getLines(int startRow, int numRows, std::vector<std::string>& lines) const;
+	void undo();
+
+private:
+		// Data Members
+	std::list<std::list<char>> m_text;
+	std::list<std::list<char>>::iterator m_curPos;
+	int m_totalLines;	// Is initialization 1 or 0 lines
+
+		// Functions
+	std::list<std::list<char>>::iterator getFirstPos();
+};
+
+#endif // STUDENTTEXTEDITOR_H_
