2ce6924acb9dca4c76a94be50eab94aa798777aa
minotaur487 <63748624+minotaur487@users.noreply.github.com>
2021-03-06 21:32:27 -0500

    implemented undo. still needs testing

--- Wurd/StudentUndo.cpp
+++ Wurd/StudentUndo.cpp
@@ -1,18 +1,72 @@
 #include "StudentUndo.h"
+#include <stack>
 
 Undo* createUndo()
 {
 	return new StudentUndo;
 }
 
-void StudentUndo::submit(const Action action, int row, int col, char ch) {
-	// TODO
+// O(1) avg case, may increase up to O(current line length where operation occurred) infrequently
+void StudentUndo::submit(const Action action, int row, int col, char ch)
+{
+	change newChange = change(action, row, col, ch);	// what if user deletes a couple times, inserts, undos, then deletes?		!!!
+	switch (action)
+	{
+	// need to implement accounting for deleting and backspacing at end of line			!!!
+	case Action::DELETE:
+	{
+		batchIfApplicable(newChange, ch);		// O(L) where L is length of top.m_changes
+		return;
+	}
+	case Action::INSERT:
+	{
+		batchIfApplicable(newChange, ch);	// O(L) where L is length of top.m_changes
+		return;
+	}
+	}
+	m_undoStack.push(newChange);	// O(1)
 }
 
-StudentUndo::Action StudentUndo::get(int &row, int &col, int& count, std::string& text) {
-    return Action::ERROR;  // TODO
+// O(1) avg case, may increase to O(L of deleted char that need to be restored) for restoration cases
+StudentUndo::Action StudentUndo::get(int &row, int &col, int& count, std::string& text) 
+{
+	if (m_undoStack.empty())
+		return Action::ERROR;
+
+	change top = m_undoStack.top();
+	row = top.m_row;
+	col = top.m_col;
+
+	if (top.m_action == Action::INSERT)
+		count = top.m_changes.size();
+	else
+		count = 1;			// for some reason, spec doesn't include batching of inserts?		!!!
+	
+	if (top.m_action == Action::DELETE)
+		text = top.m_changes;	// O(L)
+	else
+		text = "";
+
+	// undo operation removed
+	m_undoStack.pop();
+
+	switch (top.m_action)
+	{
+	case INSERT:
+		return Action::DELETE;
+	case DELETE:
+		return Action::INSERT;
+	case SPLIT:
+		return Action::JOIN;
+	case JOIN:
+		return Action::SPLIT;
+	}
 }
 
-void StudentUndo::clear() {
-	// TODO
+void StudentUndo::clear() 
+{
+	while (!m_undoStack.empty())	// O(N)
+	{
+		m_undoStack.pop();
+	}
 }
--- Wurd/StudentUndo.h
+++ Wurd/StudentUndo.h
@@ -3,6 +3,9 @@
 
 #include "Undo.h"
 
+#include <stack>
+#include <string>
+
 class StudentUndo : public Undo {
 public:
 
@@ -11,7 +14,60 @@ public:
 	void clear();
 
 private:
+	struct change
+	{
+		change(Action action, int row, int col, char ch = 0)
+			: m_row(row), m_col(col), m_action(action)
+		{
+			m_changes = std::string(1, ch);
+		}
 
+		bool operator==(const change& prevChange) const
+		{
+			if (m_col == prevChange.m_col && m_row == prevChange.m_row && m_action == prevChange.m_action)
+				return true;
+			return false;
+		}
+		
+			// data members
+		int m_row, m_col;
+		std::string m_changes;
+		Action m_action;
+	};
+
+		// functions that do
+	void addToChanges(change& curBatch, char ch) { curBatch.m_changes += ch; }
+	void batchIfApplicable(change& newChange, char ch);
+
+		// functions that get
+	int getChangeRow(const change& curChange) const { return curChange.m_row; }
+	int getChangeCol(const change& curChange) const { return curChange.m_col; }
+	Action getChangeAction(const change& curChange) const { return curChange.m_action; }
+	std::string getChanges(const change& curChange) const { return curChange.m_changes; }
+
+		// data members
+	std::stack<change> m_undoStack;
 };
+
+inline
+void StudentUndo::batchIfApplicable(StudentUndo::change& newChange, char ch)
+{
+	if (!m_undoStack.empty())
+	{
+		change top = m_undoStack.top();
+		int colDiff = top.m_col - top.m_changes.size() - newChange.m_col;
+
+		if (top == newChange)
+		{
+			addToChanges(top, ch);	// O(L) where L is length of top.m_changes
+			return;
+		}
+		else if (newChange.m_action == INSERT && top.m_action == INSERT && colDiff == 0)
+		{
+			addToChanges(top, ch);	// O(L) where L is length of top.m_changes
+			return;
+		}
+	}
+}
 
 #endif // STUDENTUNDO_H_
